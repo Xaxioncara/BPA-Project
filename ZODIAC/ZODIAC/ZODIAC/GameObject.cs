cc6ad0d8ba919677f7d38bc72d0106b1f2c4cac4
Jake Halopoff <xaxioncara@gmail.com>
2012-11-17 15:21:55 -0700

    Game Start
    Started Coding our GameObject subclass, can't figure out how to Call the
    class and reuse it in our main game.

---
+++ ZODIAC/ZODIAC/ZODIAC/GameObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ZODIAC
+{
+    public class GameBro
+    {
+
+        public static GameBro()
+        {
+             int ATTK = 0;
+            int HP = 0;
+            string ELEMENT = null;
+            Vector2 UnitPosition = new Vector2(0, 0);
+            Texture2D Texture;
+           
+        }   
+    }
+}
